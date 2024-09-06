5370dd8862c14a7a69be4a2b2b977260dceabe19
NxPKG <iconmamundentist@gmail.com>
2024-09-06 11:36:55 +0600

    Create build_alpine_arm.sh
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ scripts/build_alpine_arm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apk add musl-dev build-base
+pip install -r requirements.txt
+pip install .
+python setup.py build
+STATICPR_LIB_PATH=$(find build/ -maxdepth 1 -type d -name 'lib.*' -print -quit | xargs -I {} sh -c "find {} -type f -name 'staticpr*' -print -quit | sed 's|^./||'")
+pip install pyinstaller
+pyinstaller --add-binary ${STATICPR_LIB_PATH}:. --copy-metadata pr-action --hidden-import staticpr_languages -F pr_action/main.py
+cp ./dist/main ./dist/praction_$1
